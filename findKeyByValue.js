d905169c0e7158830a4538411d8eb64c9fcafa7f
Eduardo <eduardocastro.a@hotmail.com>
2022-05-04 22:23:34 +0000

    findkeysvalues assignment completed

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log(String.fromCodePoint(0x1f44e).repeat(3) + `Assertion Failed: ${actual} !== ${expected}`);
+    return;
+  }
+
+  console.log(String.fromCodePoint(0x1f44c).repeat(3) + `Assertion Passed: ${actual} === ${expected}`);
+};
+
+
+const findKeyByValue = function(object, value) {
+  const keys = Object.keys(object)
+  
+  for (let x of keys) {
+    if (object[x] === value) {
+      console.log(x);
+      return x
+    }
+  }
+
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const key = Object.keys(bestTVShowsByGenre);
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "drama");
