ff03555d26140a923483d6cb8df84bc251f61578
Eduardo <eduardocastro.a@hotmail.com>
2022-05-09 22:45:39 +0000

    refactored the file with module

--- head.js
+++ head.js
@@ -1,4 +1,3 @@
-const assertEqual = require('./assertEqual');
 
 // TEST CODE
 
@@ -9,7 +8,5 @@ const head = function(array) {
   
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
-
+module.exports = head;
