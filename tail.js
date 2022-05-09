0924814baa43a916c9239be4a898eaa604adfc42
Eduardo <eduardocastro.a@hotmail.com>
2022-05-09 20:57:38 +0000

    refactored and removed test

--- tail.js
+++ tail.js
@@ -9,20 +9,4 @@ const tail = function(array) {
 };
 
 
-
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const test = [];
-const test2 = ['car'];
-
-console.log(tail(words));
-console.log(tail(test));
-console.log(tail(test2));
-
-console.log(test2);
-
-
+module.exports = tail;
