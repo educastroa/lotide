1cf1576ee0b663dfda30a311bd0c4a71da0d58b7
Eduardo <eduardocastro.a@hotmail.com>
2022-05-09 21:43:53 +0000

    refactored and exported module

---
+++ test/assertArrayEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArrayEqual');
+
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
